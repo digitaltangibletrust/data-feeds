cc25f1fbe7c784dc1b80dbbfe9f3a1886e70c7ec
Bolgov Roman <bolgovr@gmail.com>
2014-07-24 01:30:01 +0300

    prev++

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var restify = require("restify");
 var config = require("config");
 var server = restify.createServer(config.app);
-var res = require("./fetchers/index.js");
-res.events.on("result", function (result) {
+var res = require("./fetchers/index.js").spin();
+res.on("result", function (result) {
   console.dir(result);
 });
 server.listen(config.port, function () {
