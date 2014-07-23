0090e555eb50f091a26ccb3f72b01323404730d3
Bolgov Roman <bolgovr@gmail.com>
2014-07-24 01:29:38 +0300

    minor refactor

--- fetchers/index.js
+++ fetchers/index.js
@@ -6,24 +6,18 @@ var fetchers = {};
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-function render(source, view, obj) {
-	console.dir(arguments);
-}
-
 function onError(source, err) {
 	console.log("source " + source + " got error: ");
 	console.dir(err);
 }
-
-for (var source in config.sources) {
-	if (config.sources[source].active === true) {
-		fetchers[source] = {
-			"pull": require("./" + source + "/pull.js"),
-			"view": require("./" + source + "/view.js")
-		};
-		rawResults.on(source, render.bind(null, source, fetchers[source].view));
-		fetchers[source].pull(config.sources[source], rawResults, onError.bind(null, source));
+module.exports.spin = function () {
+	for (var source in config.sources) {
+		if (config.sources[source].active === true) {
+			var pull = require("./" + source + "/pull.js");
+			var view = require("./" + source + "/view.js");
+			rawResults.on(source, view.bind(null, resultBus));
+			pull(config.sources[source], rawResults, onError.bind(null, source));
+		}
 	}
-}
-
-module.exports.events = resultBus;
+	return resultBus;
+};
