b08806ea546816ed0181100364a4d2513401ffe2
Bolgov Roman <bolgovr@gmail.com>
2014-07-24 01:28:07 +0300

    output results to console

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var restify = require("restify");
 var config = require("config");
 var server = restify.createServer(config.app);
-
+var res = require("./fetchers/index.js");
+res.events.on("result", function (result) {
+  console.dir(result);
+});
 server.listen(config.port, function () {
   console.log("%s listening at %s", server.name, server.url);
 });
