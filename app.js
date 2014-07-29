9870e9cb6bcf82985fa5d240b73c51efc098c2db
Bolgov Roman <bolgovr@gmail.com>
2014-07-30 01:20:41 +0300

    introduce basic REST API design

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var restify = require("restify");
 var config = require("config");
 var server = restify.createServer(config.app);
-var res = require("./fetchers/index.js").spin();
-res.on("result", function (result) {
-  console.dir(result);
+var models = require("./models/index.js");
+server.use(restify.fullResponse());
+
+server.get("/feed", function (req, res) {
+
+});
+server.get("/feed/:exchange", function (req, res) {
+
+});
+server.get("/feed/:exchange/:token", function (req, res) {
+
+});
+server.get("/tokens", function (req, res) {
+
+});
+server.get("/exchanges", function (req, res) {
+
 });
 server.listen(config.port, function () {
   console.log("%s listening at %s", server.name, server.url);
