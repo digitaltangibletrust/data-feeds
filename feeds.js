adf2661957a7e4294d6366b39e0e2880d5fc69e7
Bolgov Roman <bolgovr@gmail.com>
2014-07-30 02:03:19 +0300

    introduce sync_db command

--- feeds.js
+++ feeds.js
@@ -1,5 +1,17 @@
 var config = require("config");
 var models = require("./models/index.js");
+if (process.env.SYNC_DB) {
+  return models.sequelize.sync({
+    "force": true
+  }).complete(function (err) {
+    console.log("done");
+    if (err) {
+      console.dir(err);
+      return process.exit(1);
+    }
+    process.exit(0);
+  });
+}
 var res = require("./fetchers/index.js").spin();
 res.on("result", function (result) {
   if (result.bid > 0) {
