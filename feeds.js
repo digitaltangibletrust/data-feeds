6c9ad5b25575e5437fca7910f46e12c146e8c504
Curtis Lacy <curtis@digitaltangibletrust.com>
2014-09-05 22:52:24 -0400

    When we add a data point to the database, if it's a price in USD, go ahead and convert it to BTC at that point in time too, using the most current BTC price we have at that point.

--- feeds.js
+++ feeds.js
@@ -25,6 +25,31 @@ if (process.env.CREATE_VIEWS) {
 var res = require("./fetchers/index.js").spin();
 res.on("result", function (result) {
   if (result.bid > 0) {
+    if( result.token.substring( result.token.length - 3 ) !== 'BTC' && 
+        result.token.substring( 0, 3 ) !== 'BTC' ) {
+
+      models.data.getLatestPrices("USDtoBTC").complete(function (err, data) {
+        var btcPrice = data[0].ask + data[0].bid / 2.0;
+        if (err) {
+          console.dir(err);
+        } else {
+          var convertedData = {
+            "source": result.source,
+            "token": result.token.substring( 0, result.token.length - 3 ) + 'BTC',
+            "bid": result.bid / btcPrice,
+            "ask": result.ask / btcPrice,
+            "low": result.low / btcPrice,
+            "high": result.high / btcPrice
+          };
+          models.data.create(convertedData).complete(function (err) {
+            if (err) {
+              console.dir(err);
+            }
+          });
+        }
+      });
+
+    }
     models.data.create(result).complete(function (err) {
       if (err) {
         console.dir(err);
