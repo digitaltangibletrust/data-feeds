3a8cdc96f964019332b4598163e2e37389a8dc93
Karl Keefer <karlkeefer@gmail.com>
2014-09-24 12:47:57 -0700

    use spaces

--- fetchers/melotic/view.js
+++ fetchers/melotic/view.js
@@ -3,16 +3,16 @@ var currencyOne = "BTC";
 
 module.exports = function (resultBus, obj) {
 
-	// GLDPAMPBAROZ market.
-	var data = obj[ 'gold-btc' ];
-	if( data ) {
-		resultBus.emit( "result", {
-			"source": name,
-			"token": "GLDPAMPBAROZtoBTC",
-			"bid": parseFloat( data.highest_bid ),
-			"ask": parseFloat( data.lowest_ask ),
-			"low": parseFloat( data.lowest_deal_price ),
-			"high": parseFloat( data.highest_deal_price )
-		});
-	}
+  // GLDPAMPBAROZ market.
+  var data = obj[ 'gold-btc' ];
+  if( data ) {
+    resultBus.emit( "result", {
+      "source": name,
+      "token": "GLDPAMPBAROZtoBTC",
+      "bid": parseFloat( data.highest_bid ),
+      "ask": parseFloat( data.lowest_ask ),
+      "low": parseFloat( data.lowest_deal_price ),
+      "high": parseFloat( data.highest_deal_price )
+    });
+  }
 };
