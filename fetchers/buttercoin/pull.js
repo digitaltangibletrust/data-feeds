c6e4f48a298685887df573b89f28a483f4ae9ddc
Karl Keefer <karl@karlkeefer.com>
2014-11-17 11:15:59 -0800

    emit event and setTimeout for buttercoin

--- fetchers/buttercoin/pull.js
+++ fetchers/buttercoin/pull.js
@@ -9,7 +9,15 @@ buttercoin = Buttercoin(fakeAPIKey, fakeAPIKey, 'production', 'v1');
 module.exports = function (params, rawResults, callback) {
 
   function fetch(callback) {
-    buttercoin.getTicker(callback);
+    buttercoin.getTicker(function (err, data){
+      if (err && err.code !== "ETIMEDOUT" && err.code !== "ECONNRESET") {
+        return callback(err);
+      }
+      else if(!err && data) {
+        rawResults.emit("buttercoin", data);
+      }
+      setTimeout(callback, params.interval);
+    });
   }
 
   async.forever(fetch, callback);
