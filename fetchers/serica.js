30ea81020efe712433814882e4824768e8afc2ec
karlkeefer <karl@karlkeefer.com>
2015-04-21 22:11:49 -0700

    reduce loops and callbacks with a master async.reduce

--- fetchers/serica.js
+++ fetchers/serica.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var request = require('request');
 var _ = require('lodash');
 var async = require('async');
 
@@ -10,61 +9,46 @@ module.exports = function(apiParams, source, models) {
       cb(results);
     },
     'pull': function(cb) {
-      async.map(apiParams.feeds, function(feed, cb) {
-        var fns = _.map(feed.weights, function(weight, exchange) {
-          return function(cb) {
-            models.data.getData({
-              'resolution': 'data_1min',
-              'exchange': exchange,
-              'token': feed.token,
-              'order': 'DESC',
-              'limit': 1,
-            }).complete(function(err, data) {
-              if(err) {
-                return cb(err);
-              }
-
-              data = data[0];
-              data.weight = weight;
-
-              cb(null, data);
-            });
-          };
-        });
-        async.parallel(fns, function(err, results) {
-          if(err) {
-            return cb(err);
-          }
-
-          var composite = _.reduce(results, function(_composite, data) {
-            _composite.bid += data.bid * data.weight;
-            _composite.ask += data.ask * data.weight;
-            _composite.low += data.low * data.weight;
-            _composite.high += data.high * data.weight;
-            return _composite;
-          }, {
-            'source': source,
-            'token': feed.token,
-            'bid': 0,
-            'ask': 0,
-            'low': 0,
-            'high': 0
-          });
-
-          var totalWeight = _.reduce(feed.weights, function(sum, weight) {
-            return sum + weight;
-          }, 0);
-
-          composite.bid /= totalWeight;
-          composite.ask /= totalWeight;
-          composite.low /= totalWeight;
-          composite.high /= totalWeight;
-
-          cb(null, composite);
-        });
-      }, function(err, results) {
-        cb(err, null, results);   // callback with request format
+      async.map(apiParams.feeds, processFeed, function(err, results) {
+        cb(err, null, results); // callback with request format
       });
     }
   };
+
+  // for each config.serica.feeds
+  function processFeed(feed, cb) {
+    var totalWeight = _.reduce(feed.weights, function(sum, weight) {
+      return sum + weight;
+    }, 0);
+
+    async.reduce(Object.keys(feed.weights), {
+      'source': source,
+      'token': feed.token,
+      'bid': 0,
+      'ask': 0,
+      'low': 0,
+      'high': 0
+    }, function(composite, exchange, reduceCB) {
+      models.data.getData({
+        'resolution': 'data_1min',
+        'exchange': exchange,
+        'token': feed.token,
+        'order': 'DESC',
+        'limit': 1,
+      }).complete(function(err, data) {
+        if (err) {
+          return reduceCB(err);
+        }
+
+        var relativeWeight = feed.weights[exchange] / totalWeight;
+
+        composite.bid += data[0].bid * relativeWeight;
+        composite.ask += data[0].ask * relativeWeight;
+        composite.low += data[0].low * relativeWeight;
+        composite.high += data[0].high * relativeWeight;
+
+        reduceCB(null, composite);
+      });
+    }, cb);
+  }
 };
