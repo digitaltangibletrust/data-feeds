a31f4ca2d81be499c82b617db5f42c72da8a2902
porkchop <porkchop@captureplay.com>
2015-04-22 22:12:32 -0700

    Instance methodized spreads calculation

--- models/sericafeed.js
+++ models/sericafeed.js
@@ -1,5 +1,6 @@
 'use strict';
 var _ = require('lodash');
+var async = require('async');
 
 module.exports = function (sequelize, DataTypes) {
   var globalModels;
@@ -10,7 +11,7 @@ module.exports = function (sequelize, DataTypes) {
       "primaryKey": true,
       "autoIncrement": true
     },
-    // Can be reused to signify a containging feed for tokens bid/ask's
+    // Can be reused to signify a containing feed for tokens bid/ask's
     "name": {
       "type": DataTypes.STRING,
       "allowNull": false,
@@ -63,6 +64,65 @@ module.exports = function (sequelize, DataTypes) {
       "getWeight": function (exchange) {
         var subfeed = _.find(JSON.parse(this.subfeeds), {'exchange': exchange});
         return subfeed.weight;
+      },
+      "calcSpreads": function(cb) {
+        var feed = this;
+        var totalWeight = 0;
+
+        var composite = {
+          'source': feed.name,
+          'token': feed.token,
+          'bid': 0,
+          'ask': 0,
+          'low': 0,
+          'high': 0
+        };
+
+        async.each(feed.getExchanges(), function(exchange, eachCB) {
+          globalModels.data.getData({
+            'resolution': 'data_1min',
+            'exchange': exchange,
+            'token': feed.token,
+            'order': 'DESC',
+            'minutesAgo': feed.subfeed_is_stale_minutes,
+            'limit': 1,
+          }).complete(function(err, data) {
+            if (err) {
+              return eachCB(err);
+            }
+
+            //If we don't see the feed it is likely stale
+            if(data && data[0]) {
+              composite.bid += data[0].bid;
+              composite.ask += data[0].ask;
+              composite.low += data[0].low;
+              composite.high += data[0].high;
+              totalWeight += feed.getWeight(exchange);
+            }
+
+
+            eachCB();
+          });
+        }, function(err) {
+          if (err) {
+            return cb(err);
+          }
+
+          // normalize
+          composite.bid /= totalWeight;
+          composite.ask /= totalWeight;
+          composite.low /= totalWeight;
+          composite.high /= totalWeight;
+
+          // adjust by the serica bid and ask premium
+          composite.bid *= (1 - feed.bid_premium / 100);
+          composite.ask *= (1 + feed.ask_premium / 100);
+          composite.low *= (1 - feed.bid_premium / 100);
+          composite.high *= (1 + feed.ask_premium / 100);
+
+
+          cb(null, composite);
+        });
       }
     }
   });
