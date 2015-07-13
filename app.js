e59d4719f45a630998a3b0c68eb13e38b323ddd3
karlkeefer <karl@karlkeefer.com>
2015-07-13 16:47:43 -0700

    add a live endpoint for serica_rates

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-"use strict";
-var restify = require("restify");
-var config = require("config");
+'use strict';
+var restify = require('restify');
+var config = require('config');
 var server = restify.createServer(config.app);
-var models = require("./models/index.js");
-var request = require( "request" );
-var async = require( 'async' );
-var _ = require( 'lodash' );
+var models = require('./models/index.js');
+var async = require('async');
+var request = require('request');
+var _ = require('lodash');
 
-var errbit = require("./errbit");
+var errbit = require('./errbit');
 errbit.handleExceptions();
 
 server.use(restify.fullResponse());
@@ -15,21 +15,19 @@ server.use(restify.jsonp());
 server.use(restify.gzipResponse());
 server.use(restify.queryParser());
 server.use(restify.throttle({
-  "burst": 100,
-  "rate": 50,
-  "ip": true,
-  "overrides": {
-    "192.168.1.1": {
-      "rate": 0, // unlimited
-      "burst": 0
+  'burst': 100,
+  'rate': 50,
+  'ip': true,
+  'overrides': {
+    '192.168.1.1': {
+      'rate': 0, // unlimited
+      'burst': 0
     }
   }
 }));
 
-var availableIntervals = ["day", "hour", "month"];
-var defaultInterval = "hour";
-
-var liveData = {};
+var availableIntervals = ['day', 'hour', 'month'];
+var defaultInterval = 'hour';
 
 function validateInterval(interval) {
   if (!interval) {
@@ -41,11 +39,14 @@ function validateInterval(interval) {
   return interval;
 }
 
-server.get("/live/:exchange", function(req, res, next) {
+// TODO: after the change to serica_rates goes live in the app we can strip the bitfinex stuff out here
+var liveData = {};
+server.get('/live/:exchange', function(req, res, next) {
   var liveDataPoint = liveData[req.params.exchange];
   if (liveDataPoint && (Date.now() - liveDataPoint.timestamp) < 10000) {
     return res.send(liveDataPoint);
   }
+
   switch (req.params.exchange) {
     case 'bitfinex':
       request.get('https://api.bitfinex.com/v1/pubticker/BTCUSD', function(err, response, data) {
@@ -59,45 +60,41 @@ server.get("/live/:exchange", function(req, res, next) {
         try {
           dataObj = JSON.parse(data);
         } catch (e) {
-          errbit.notify(new Error("data-feeds api could not parse bitfinex ticker response: " + data));
+          errbit.notify(new Error('data-feeds api could not parse bitfinex ticker response: ' + data));
           return res.send(500);
         }
 
         dataObj.timestamp = Date.now();
         liveData[req.params.exchange] = dataObj;
-        res.send(dataObj);
+        return res.send(dataObj);
       });
       break;
-    default:
-      res.send(404);
-  }
-}, function databaseFallback(req, res, next) {
-  switch (req.params.exchange) {
-    case 'bitfinex':
-      models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE source=? AND token= ? ORDER BY created_at DESC LIMIT 1", null, {
-        "raw": true
-      }, ['bitfinex', 'USDtoBTC']).complete(function(err, data) {
-        // this fallback from the database doesn't mirror the live 
-        // response perfectly, but it covers all values we're likely to use
+    case 'serica':
+      models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token= ? ORDER BY created_at DESC LIMIT 1', null, {
+        'raw': true
+      }, ['serica', 'USDtoBTC']).complete(function(err, data) {
         var latest = _.first(data);
-        res.send({
-          'mid': (latest.bid + latest.ask / 2).toFixed(4),
-          'bid': latest.bid.toFixed(4),
-          'ask': latest.ask.toFixed(4),
+        var dataObj = {
+          'bid': Number(latest.bid.toFixed(4)),
+          'ask': Number(latest.ask.toFixed(4)),
+          'rate': (latest.bid + latest.ask) / 2,
           'timestamp': Date.parse(latest.date_trunc)
-        });
+        };
+        liveData[req.params.exchange] = dataObj;
+        return res.send(dataObj);
       });
       break;
     default:
-      res.send(404);
+      return res.send(404);
   }
 });
 
-server.get("/feed", function (req, res, next) {
+
+server.get('/feed', function (req, res, next) {
   var interval = validateInterval(req.params.interval);
-  var params = ["1 " + interval];
-  models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE created_at > (NOW() - ?::interval) ORDER BY created_at DESC", null, {
-    "raw": true
+  var params = ['1 ' + interval];
+  models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE created_at > (NOW() - ?::interval) ORDER BY created_at DESC', null, {
+    'raw': true
   }, params).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -107,11 +104,11 @@ server.get("/feed", function (req, res, next) {
   });
 });
 
-server.get("/feed/:exchange", function (req, res, next) {
+server.get('/feed/:exchange', function (req, res, next) {
   var interval = validateInterval(req.params.interval);
-  var params = [req.params.exchange, "1 " + interval];
-  models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE source=? AND created_at > (NOW() - ?::interval) ORDER BY created_at DESC", null, {
-    "raw": true
+  var params = [req.params.exchange, '1 ' + interval];
+  models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE source=? AND created_at > (NOW() - ?::interval) ORDER BY created_at DESC', null, {
+    'raw': true
   }, params).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -121,11 +118,11 @@ server.get("/feed/:exchange", function (req, res, next) {
   });
 });
 
-server.get("/feed/:exchange/:token", function (req, res, next) {
+server.get('/feed/:exchange/:token', function (req, res, next) {
   var interval = validateInterval(req.params.interval);
-  var params = [req.params.exchange, req.params.token, "1 " + interval];
-  models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE source=? AND token= ? AND created_at > (NOW() - ?::interval) ORDER BY created_at DESC", null, {
-    "raw": true
+  var params = [req.params.exchange, req.params.token, '1 ' + interval];
+  models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token= ? AND created_at > (NOW() - ?::interval) ORDER BY created_at DESC', null, {
+    'raw': true
   }, params).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -135,7 +132,7 @@ server.get("/feed/:exchange/:token", function (req, res, next) {
   });
 });
 
-server.get("/highstockfeed/:exchange/:token", function (req, res, next) {
+server.get('/highstockfeed/:exchange/:token', function (req, res, next) {
   if( req.query.start && req.query.end ) {
     var start = req.query.start;
     var end = req.query.end;
@@ -143,8 +140,8 @@ server.get("/highstockfeed/:exchange/:token", function (req, res, next) {
     recoverRange( parseInt( start ), parseInt( end ) );
   } else {
     models.sequelize.query( 
-      "SELECT source as exchange, date_trunc('second', created_at) FROM data WHERE source=? AND token=? ORDER BY created_at ASC LIMIT 1", 
-      null, { "raw": true }, 
+      'SELECT source as exchange, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token=? ORDER BY created_at ASC LIMIT 1', 
+      null, { 'raw': true }, 
       [ req.params.exchange, req.params.token ] ).complete( function( err, data ) {
         if( err ) {
           return next( err );
@@ -152,8 +149,8 @@ server.get("/highstockfeed/:exchange/:token", function (req, res, next) {
 
         var start = Date.parse( data[0].date_trunc );
         models.sequelize.query( 
-          "SELECT source as exchange, date_trunc('second', created_at) FROM data WHERE source=? AND token=? ORDER BY created_at DESC LIMIT 1", 
-          null, { "raw": true }, 
+          'SELECT source as exchange, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token=? ORDER BY created_at DESC LIMIT 1', 
+          null, { 'raw': true }, 
           [ req.params.exchange, req.params.token ] ).complete( function( err, data ) {
             if( err ) {
               return next( err );
@@ -178,8 +175,8 @@ server.get("/highstockfeed/:exchange/:token", function (req, res, next) {
     async.forEach( result,
       function( item, done ) {
         var params = [ req.params.exchange, req.params.token, item.target ];
-        models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE source=? AND token= ? AND created_at <= ? ORDER BY created_at DESC LIMIT 1", null, {
-          "raw": true
+        models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token= ? AND created_at <= ? ORDER BY created_at DESC LIMIT 1', null, {
+          'raw': true
         }, params).complete(function (err, data) {
           if (err) {
             return done(err);
@@ -209,10 +206,10 @@ server.get("/highstockfeed/:exchange/:token", function (req, res, next) {
 });
 
 // Returns the most recent price item prior to a given targetTime, specified in milliseconds.
-server.get("/nearest/:exchange/:token", function (req, res, next) {
+server.get('/nearest/:exchange/:token', function (req, res, next) {
   var params = [ req.params.exchange, req.params.token, new Date( parseInt( req.params.targetTime )) ];
-  models.sequelize.query("SELECT source as exchange, token, bid, ask, low, high, date_trunc('second', created_at) FROM data WHERE source=? AND token= ? AND created_at < ? ORDER BY created_at DESC LIMIT 1", null, {
-    "raw": true
+  models.sequelize.query('SELECT source as exchange, token, bid, ask, low, high, date_trunc(\'second\', created_at) FROM data WHERE source=? AND token= ? AND created_at < ? ORDER BY created_at DESC LIMIT 1', null, {
+    'raw': true
   }, params).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -223,9 +220,9 @@ server.get("/nearest/:exchange/:token", function (req, res, next) {
   next();
 });
 
-server.get("/tokens", function (req, res, next) {
-  models.sequelize.query("SELECT DISTINCT token FROM data", null, {
-    "raw": true
+server.get('/tokens', function (req, res, next) {
+  models.sequelize.query('SELECT DISTINCT token FROM data', null, {
+    'raw': true
   }).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -235,9 +232,9 @@ server.get("/tokens", function (req, res, next) {
   });
 });
 
-server.get("/exchanges", function (req, res, next) {
-  models.sequelize.query("SELECT DISTINCT source as exchange FROM data", null, {
-    "raw": true
+server.get('/exchanges', function (req, res, next) {
+  models.sequelize.query('SELECT DISTINCT source as exchange FROM data', null, {
+    'raw': true
   }).complete(function (err, data) {
     if (err) {
       return next(err);
@@ -248,5 +245,5 @@ server.get("/exchanges", function (req, res, next) {
 });
 
 server.listen(config.port, function () {
-  console.log("%s listening at %s", server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
